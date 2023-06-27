7001697be39e4374038f24ee2bf4803576eceda5
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-06-27 23:46:41 +0300

    homep

--- src/components/Layout/NavigationHeader/NavigationHeader.jsx
+++ src/components/Layout/NavigationHeader/NavigationHeader.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import css from './NavigationHeader.module.css';
 import { logOut } from '../../../redux/authOperations';
+import { NavLink } from 'react-router-dom';
 
 const NavigationHeader = () => {
   const dispatch = useDispatch();
@@ -8,7 +9,16 @@ const NavigationHeader = () => {
   return (
     <div className={css.header}>
       <h1>PhoneBook</h1>
-      <button type="button" onClick={() => dispatch(logOut())}>Log Out</button>
+      <ul className={css.nav}>
+        <li>
+          <NavLink to="/contacts">Contacts</NavLink>
+        </li>
+        <li>
+          <button type="button" onClick={() => dispatch(logOut())}>
+            Log Out
+          </button>
+        </li>
+      </ul>
     </div>
   );
 };
